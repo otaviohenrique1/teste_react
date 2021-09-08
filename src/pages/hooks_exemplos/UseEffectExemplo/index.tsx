ab9909d62a6db94615db14cf7d8b35059dcda317
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-09-08 14:18:50 -0300

    Commit 2 dos arquivos

--- src/pages/hooks_exemplos/UseEffectExemplo/index.tsx
+++ src/pages/hooks_exemplos/UseEffectExemplo/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Table } from "reactstrap";
 import styled from "styled-components";
 import { ContainerBotoes, BotaoLink } from "../../../components/Botao";
-import { Titulo } from "../../../components/Titulo";
+import { SubTitulo, Titulo } from "../../../components/Titulo";
 import { listaDadosBrasil } from "./listaEstadosBrasil";
 
 interface DataTypes {
@@ -12,72 +12,24 @@ interface DataTypes {
   capital: string;
 }
 
-const initialData: DataTypes = {
-  bandeira: '',
-  nome: '',
-  sigla: '',
-  capital: '',
-};
-
 export function UseEffectExemplo() {
-  const [dataBrasil, setDataBrasil] = useState<DataTypes>(initialData);
+  const [dataBrasil, setDataBrasil] = useState<DataTypes[]>([]);
   const [dataEstadosBrasil, setDataEstadosBrasil] = useState<DataTypes[]>([]);
 
   useEffect(() => {
-    const data = {
-      bandeira: listaDadosBrasil[0].bandeira,
-      nome: listaDadosBrasil[0].nome,
-      sigla: listaDadosBrasil[0].sigla,
-      capital: listaDadosBrasil[0].capital,
-    };
-    setDataBrasil(data);
+    setDataBrasil(listaDadosBrasil);
     setDataEstadosBrasil(listaDadosBrasil[0].estados);
   }, []);
 
   return (
     <div>
-      <Titulo titulo="UseStateExemplo" />
-      <Table>
-        <thead>
-          <tr>
-            <td>Bandeira</td>
-            <td>Nome</td>
-            <td>Sigla</td>
-            <td>Capital</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><ImagemEstilizada src={dataBrasil.bandeira} alt={dataBrasil.nome} /></td>
-            <td>{dataBrasil.nome}</td>
-            <td>{dataBrasil.sigla}</td>
-            <td>{dataBrasil.capital}</td>
-          </tr>
-        </tbody>
-      </Table>
-      <br />
-      <Table>
-        <thead>
-          <tr>
-            <td>Bandeira</td>
-            <td>Nome</td>
-            <td>Sigla</td>
-            <td>Capital</td>
-          </tr>
-        </thead>
-        <tbody>
-          {dataEstadosBrasil.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td><ImagemEstilizada src={item.bandeira} alt={item.nome} /></td>
-                <td>{item.nome}</td>
-                <td>{item.sigla}</td>
-                <td>{item.capital}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      <Titulo titulo="UseEffectExemplo" />
+      <SubTitulo titulo="Pais"/>
+      <Tabela data={dataBrasil} />
+      <Separador />
+      <SubTitulo titulo="Unidades federativas"/>
+      <Tabela data={dataEstadosBrasil} />
+      <Separador />
       <ContainerBotoes>
         <BotaoLinkEstilizado
           to="/hooks_exemplos"
@@ -90,6 +42,11 @@ export function UseEffectExemplo() {
   );
 }
 
+const Separador = styled.hr`
+  margin-top: 50px;
+  margin-bottom: 50px;
+`;
+
 const ImagemEstilizada = styled.img`
   width: 200px;
 `;
@@ -97,4 +54,41 @@ const ImagemEstilizada = styled.img`
 const BotaoLinkEstilizado = styled(BotaoLink)`
   width: 100px;
   padding: 10px;
+`;
+
+interface TabelaProps {
+  data: DataTypes[];
+}
+
+function Tabela(props: TabelaProps) {
+  return (
+    <Table>
+        <thead>
+          <TrEstilizado>
+            <td>Bandeira</td>
+            <td>Nome</td>
+            <td>Sigla</td>
+            <td>Capital</td>
+          </TrEstilizado>
+        </thead>
+        <tbody>
+          {props.data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td><ImagemEstilizada src={item.bandeira} alt={item.nome} /></td>
+                <td>{item.nome}</td>
+                <td>{item.sigla}</td>
+                <td>{item.capital}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+  );
+}
+
+const TrEstilizado = styled.tr`
+  td {
+    font-weight: bold;
+  }
 `;
